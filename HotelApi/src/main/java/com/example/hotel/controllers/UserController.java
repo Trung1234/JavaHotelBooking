94f1a6385ebe078da5852072e67e7603fe43f9bc
Trung1234 <trungsieunhan24@gmail.com>
2024-09-22 15:35:06 +0700

    add CORS config

--- HotelApi/src/main/java/com/example/hotel/controllers/UserController.java
+++ HotelApi/src/main/java/com/example/hotel/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,7 @@ public class UserController {
 
     }
 
+//    @CrossOrigin(origins = "http://localhost:3000")  // Allow specific origin
     @PostMapping("/login")
     public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto) {
         User user = userService.authenticateUser(loginDto);
